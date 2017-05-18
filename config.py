fabd35b7501b3d360c5ab84c859d4cb6291c0483
LeeW <cl87654321@gmail.com>
2017-05-18 19:17:42 +0800

    Load config secrets in TestConfig

--- config.py
+++ config.py
@@ -28,6 +28,10 @@ class DevelopmentConfig(Config):
 
 
 class TestingConfig(Config):
+    try:
+        from config_secret import BOT_API_TOKEN, SECRET_KEY
+    except ModuleNotFoundError as error:
+        print('config_secret does not exist')
     SQLALCHEMY_DATABASE_URI = (
         os.environ.get('TEST_DATABASE_URL') or
         'sqlite:///' + os.path.join(BASE_DIR, 'data-test.sqlite')
