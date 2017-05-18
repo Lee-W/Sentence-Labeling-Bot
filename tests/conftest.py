23783b547263ef101d9e657ba7db2a6df6ba9f30
LeeW <cl87654321@gmail.com>
2017-05-18 19:42:14 +0800

    Add db, session fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,46 @@
 import pytest
-from flask import current_app
 
 from labelingbot import create_app
+from labelingbot import db as _db
 
 
-@pytest.fixture
+@pytest.fixture(scope='session')
 def app():
     app = create_app('testing')
-    return app
+    app_context = app.app_context()
+    app_context.push()
+
+    yield app
+
+    app_context.pop()
 
 
-def test_app_exists():
-    assert current_app is not None
+@pytest.fixture(scope='session')
+def db(app, request):
+    _db.app = app
+    _db.create_all()
+
+    yield _db
+
+    _db.drop_all()
+
+
+@pytest.fixture(scope='function')
+def session(db, request):
+    connection = db.engine.connect()
+    transaction = connection.begin()
+
+    session = db.create_scoped_session(
+        options={
+            "bind": connection,
+            "binds": {}
+        }
+    )
+
+    db.session = session
+
+    yield session
+
+    transaction.rollback()
+    connection.close()
+    session.remove()
