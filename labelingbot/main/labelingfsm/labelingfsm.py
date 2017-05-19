8c6c951689b362e1d8631d79ab5be88c942a8efb
LeeW <cl87654321@gmail.com>
2017-05-19 23:44:12 +0800

    Add empty on_enter functions to labelingfsm

--- labelingbot/main/labelingfsm/labelingfsm.py
+++ labelingbot/main/labelingfsm/labelingfsm.py
@@ -2,4 +2,35 @@ from .botfsm import BotGraphMachine
 
 
 class LabelingMachine(BotGraphMachine):
-    pass
+    def on_enter_ask_usage(self, event):
+        raise NotImplementedError
+
+    def on_enter_user(self, event):
+        raise NotImplementedError
+
+    def on_enter_wait_user_alternative(self, event):
+        raise NotImplementedError
+
+    def on_enter_wait_user_binary_response(self, event):
+        raise NotImplementedError
+
+    def on_enter_wait_user_similarity_score(self, event):
+        raise NotImplementedError
+
+    def is_asking_usage(self, event):
+        raise NotImplementedError
+
+    def is_answering_alternative(self, event):
+        raise NotImplementedError
+
+    def is_answering_binary(self, event):
+        raise NotImplementedError
+
+    def is_answering_score(self, event):
+        raise NotImplementedError
+
+    def is_answering(self, event):
+        raise NotImplementedError
+
+    def is_quitting(self, event):
+        raise NotImplementedError
