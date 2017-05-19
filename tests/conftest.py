4757f69366215d7d0463a46a3ead91eaf1709ba2
LeeW <cl87654321@gmail.com>
2017-05-19 12:47:26 +0800

    Add client fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -44,3 +44,9 @@ def session(db, request):
     transaction.rollback()
     connection.close()
     session.remove()
+
+
+@pytest.fixture(scope='session')
+def client(app):
+    client = app.test_client(use_cookies=True)
+    return client
