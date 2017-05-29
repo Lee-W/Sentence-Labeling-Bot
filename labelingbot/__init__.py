fdb744e976928187bed19f6190ba3bf9ffc92d7c
LeeW <cl87654321@gmail.com>
2017-05-29 15:51:08 +0800

    Add -c option and init_db command to manage

--- labelingbot/__init__.py
+++ labelingbot/__init__.py
@@ -2,8 +2,9 @@ import os
 
 from flask import Flask
 from flask_bootstrap import Bootstrap
-from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 import telegram
 
 from config import config
@@ -16,6 +17,7 @@ APP_TEMPLATE_PATH = os.path.join(APP_ROOT_PATH, 'templates')
 bot = None
 db = SQLAlchemy()
 bootstarp = Bootstrap()
+migrate = Migrate()
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
@@ -34,6 +36,7 @@ def create_app(config_name):
     db.init_app(app)
     bootstarp.init_app(app)
     login_manager.init_app(app)
+    migrate.init_app(app, db)
 
     from .main import main as main_blueprint
     from .auth import auth as auth_blueprint
--- manage.py
+++ manage.py
@@ -1,21 +1,30 @@
 import os
 
+from flask import current_app
 from flask_script import Manager, Shell
-from flask_migrate import Migrate, MigrateCommand
+from flask_migrate import MigrateCommand
 
 from labelingbot import create_app, db
 from labelingbot.models import User
 
 
-app = create_app(os.getenv('LABELING_BOT_CONFIG') or 'default')
-manager = Manager(app)
-migrate = Migrate(app, db)
-
 def make_shell_context():
-    return dict(app=app, db=db, User=User)
+    return dict(app=current_app, db=db, User=User)
 
+
+manager = Manager(create_app)
+manager.add_option(
+    '-c', '--config', dest='config_name',
+    required=False, default=os.getenv('LABELING_BOT_CONFIG') or 'default'
+)
 manager.add_command('shell', Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
+
+
+@manager.command
+def init_db():
+    from labelingbot import models
+    db.create_all()
 
 
 if __name__ == "__main__":
